7ba36e37a54827d663608d92ba85201917a888e7
Ronen Babayoff <ronen@lavaina.com>
2015-08-05 21:31:48 -0400

    Modify package to be usable with my autoform version and change package owner to me.

--- package.js
+++ package.js
@@ -1,17 +1,23 @@
 Package.describe({
-  name: 'dangrossman:bootstrap-daterangepicker',
-  version: '2.0.6',
+  name: 'rbabayoff:bootstrap-daterangepicker',
+  version: '2.0.6_1',
   summary: 'Date range picker component for Bootstrap',
-  git: 'https://github.com/dangrossman/bootstrap-daterangepicker',
+  git: 'https://github.com/rbabayoff/bootstrap-daterangepicker',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0.1');
+  api.versionsFrom('METEOR@0.9.3');
 
-  api.use('twbs:bootstrap@3.3.4', ["client"]);
-  api.use('momentjs:moment@2.10.3', ["client"]);
-  api.use('jquery@1.11.3_2', ["client"]);
+  //api.use('twbs:bootstrap@3.3.4', ["client"]);
+  //api.use('momentjs:moment@2.10.3', ["client"]);
+  api.use(['jquery', 'twbs:bootstrap', 'momentjs:moment'], ["client"]);
+
+  api.imply([
+    'jquery',
+    'twbs:bootstrap',
+    'momentjs:moment'
+  ]);
 
   api.addFiles('daterangepicker.js', ["client"]);
   api.addFiles('daterangepicker.css', ["client"]);
